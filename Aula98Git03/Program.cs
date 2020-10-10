fd02d315c58c32e8039fde83e54967bc61c26458
motovmp1 <vinicius.mpinho@gmail.com>
2020-10-10 11:33:14 +0100

    Projeto iniciado 001

---
+++ Aula98Git03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula98Git03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello mundo");
+            //Console.WriteLine("Bom dia!");
+            //Console.WriteLine("Ola mundo 2");
+            Console.ReadKey();
+
+        }
+    }
+}
